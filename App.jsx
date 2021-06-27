0989c52a4105b78f1cb58c5f081117eccdf13da8
kerisrikrishna <86559758+kerisrikrishna@users.noreply.github.com>
2021-06-27 19:16:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+function App() {
+  return (
+    <div>
+      <Footer />
+      <Header />
+      <Info />
+    </div>
+  );
+}
+export default App;
